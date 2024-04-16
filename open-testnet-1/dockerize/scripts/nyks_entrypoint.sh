dab9777d61a4734de31d313e7ad2f2efcfed75aa
ahmad ashraf <ahmadashraf250@gmai.com>
2024-04-17 03:50:49 +0500

    fixing volume issue

--- open-testnet-1/dockerize/scripts/nyks_entrypoint.sh
+++ open-testnet-1/dockerize/scripts/nyks_entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+cp -Rv /testnet/data/* /root/.nyks/
+nyksd start
 
-cp /testnet/priv_validator_state.json /root/.nyks/data/priv_validator_state.json
-nyksd start > /dev/null 2>&1 &
-cd /testnet/btc-oracle
 # =====================================================
 #uncomment once the Chain is up to date.
-#nyksd keys show validator-self -a --keyring-backend test
-#sleep 900git stat
+# nyksd keys show validator-self -a --keyring-backend test
+# sleep 900
+# cd /testnet/btc-oracle
 # /testnet/btc-oracle/forkoracle-go --new_wallet true # --mnemonic "<12 word mnemonic>"
 # ====================================================
