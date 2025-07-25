f113c944add85995c5db9c5cd7922144d2e5aecb
ahmad ashraf <ahmadashraf250@gmai.com>
2025-07-25 07:50:55 +0100

    chain bootstrap process fix

--- open-testnet-2/validator-docker/scripts/nyks_entrypoint.sh
+++ open-testnet-2/validator-docker/scripts/nyks_entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # This script is the entrypoint for the nyksd container.
 # It copies the testnet data to the nyksd data directory and starts the nyksd daemon.
-cp -Rv /testnet/data/* /root/.nyks/
+# cp -Rv /testnet/data/* /root/.nyks/
+./pre_bootstrap.sh
 nyksd start &
-./testnet/bootstrap.sh
+sleep 15
+./bootstrap.sh
 cd /testnet/zkoracle-go
 ./zkoracle-go
 
---
+++ open-testnet-2/validator-docker/scripts/pre_bootstrap.sh
@@ -0,0 +1,16 @@
+nyksd init validator-self --chain-id nyks
+# Create and add a key named validator-self to the node
+nyksd keys add validator-self --keyring-backend test
+
+# Add genesis account and generate genesis transaction
+nyksd add-genesis-account validator-self 10000000000000nyks --keyring-backend test
+nyksd gentx validator-self 100000000000nyks --chain-id nyks --keyring-backend test 
+nyksd collect-gentxs
+
+# Update the genesis.json file to change the stake to nyks
+sed -i 's/stake/nyks/g' /root/.nyks/config/genesis.json
+sed -i '11s/.*/minimum-gas-prices = "0nyks"/' /root/.nyks/config/app.toml
+sed -i '129s/.*/enabled-unsafe-cors = true/' /root/.nyks/config/app.toml
+sed -i '108s/.*/enable = true/' /root/.nyks/config/app.toml
+sed -i '96s/.*/cors_allowed_origins = ["*"]/' /root/.nyks/config/config.toml
+sed -i '91s/.*/laddr = "tcp:\/\/0.0.0.0:26657"/' /root/.nyks/config/config.toml
