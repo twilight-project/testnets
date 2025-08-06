d9de47d16cba8994132d3b936c63828f5677acd2
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2025-08-06 23:24:27 +0000

    nyks dockerfile multi-stage-build

--- open-testnet-2/validator-docker/scripts/nyks_entrypoint.sh
+++ open-testnet-2/validator-docker/scripts/nyks_entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # Run pre_bootstrap.sh only once
 if [ ! -f "/root/.nyks/prebootstrap_done" ]; then
     echo "Running pre_bootstrap.sh for the first time..."
-    ./pre_bootstrap.sh
+    pre_bootstrap.sh
     touch /root/.nyks/prebootstrap_done
 else
     echo "pre_bootstrap.sh already completed. Skipping..."
@@ -17,17 +17,13 @@ nyksd start &
 if [ ! -f "/root/.nyks/bootstrap_done" ]; then
 sleep 15
     echo "Running bootstrap.sh for the first time..."
-    ./bootstrap.sh
+    bootstrap.sh
     touch /root/.nyks/bootstrap_done
 else
 sleep 3
     echo "bootstrap.sh already completed. Skipping..."
 fi
-# echo "Starting zkoracle-go"
-# cd /testnet/zkoracle-go
-# # exec ./ZkOracle &
-# exec ./zkoracle-go &
+
 
 echo "Starting btcDepositConfirmer"
-cd /testnet/btcDepositConfirmer
-exec ./depositconfirmer
+exec depositconfirmer
