fb123f3f81ce9ba792f88d7e1c5fa3af24de7303
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2025-10-25 12:32:27 +0000

    Add twilight-indexer and indexer-db services to docker-compose; update Dockerfiles and SQL schema

--- open-testnet-2/validator-docker/zkpass/zkpass.sql
+++ open-testnet-2/validator-docker/zkpass/zkpass.sql
@@ -12,3 +12,16 @@ CREATE TABLE IF NOT EXISTS public.selfcheck (
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_zkpass_unique ON public.zkpass (address, provider);
 ALTER DATABASE zkpass SET timezone TO 'UTC';
+
+
+CREATE TABLE IF NOT EXISTS public.addresses (
+    address VARCHAR(255) PRIMARY KEY,
+    lastusedNyks TIMESTAMP,
+    lastusedSats TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS public.AddressMappings (
+    twilightAddress VARCHAR(255) PRIMARY KEY,
+    ethAddress VARCHAR(255) UNIQUE NOT NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
